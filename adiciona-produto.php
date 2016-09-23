c3335b22960fa551b83690fff82ec8a584667d9c
rCirelli <raphaelc.garrido@gmail.com>
2016-09-22 21:42:48 -0300

    Final Aula 2

---
+++ adiciona-produto.php
@@ -0,0 +1,18 @@
+<?php include("cabecalho.php");
+
+  $nome = $_GET["nome"];
+  $preco = $_GET["preco"];
+
+  $conexao = mysqli_connect('localhost', 'root', '', 'loja');
+  $query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+
+if(mysqli_query($conexao, $query)) {
+?>
+<p class="alert-success">Produto <?= $nome; ?>, de valor R$<?= $preco; ?> adicionado com sucesso!</p>
+<?php
+}else {
+?>
+<p class="alert-danger">Produto <?= $nome; ?> não foi adicionado.</p>
+<?php
+}
+include("rodape.php") ?>
---
+++ produto-formulario.php
@@ -0,0 +1,9 @@
+<?php include("cabecalho.php") ?>
+          <h1>Formulário de cadastro</h1>
+          <form action="adiciona-produto.php">
+              Nome: <input type="text" name="nome" /><br/><br/>
+              Preço: <input type="number" name="preco" /><br/><br/>
+
+              <input type="submit" value="Cadastrar" />
+          </form>
+<?php include("rodape.php") ?>
