09ea518ba51e9854813504c1a6c34c6a320e7cb8
rCirelli <raphaelc.garrido@gmail.com>
2016-10-04 18:22:21 -0300

    Fim aula 6
    começo aula 7

--- produto-formulario.php
+++ produto-formulario.php
@@ -1,6 +1,6 @@
 <?php include("cabecalho.php"); ?>
     <h1>Formulário de cadastro</h1>
-    <form action="adiciona-produto.php">
+    <form action="adiciona-produto.php" method="post">
         <table class="table">
             <tr>
                <td>Nome</td>
@@ -9,6 +9,10 @@
             <tr>
               <td>Preço</td>
               <td><input class="form-control" type="number" name="preco"/></td>
+            </tr>
+            <tr>
+              <td>Descrição</td>
+              <td><textarea class="form-control" name="descrição"></textarea></td>
             </tr>
             <tr>
               <td><input class="btn btn-primary" type="submit" value="Cadastrar"></td>
--- produto-lista.php
+++ produto-lista.php
@@ -2,17 +2,28 @@
 include("cabecalho.php");
 include("conecta.php");
 include("banco-produto.php");
+
+if(array_key_exists("removido", $_POST) && $_POST["removido"]=="true"){
+?>
+  <p class="alert-success">Produto apagado com sucesso.</p>
+<?php
+}
 ?>
 
 <table class="table table-striped table-bordered">
 
     <?php
       $produtos = listaProdutos($conexao);
-      foreach ($produtos as $produto) :
+      foreach($produtos as $produto) :
     ?>
     <tr>
-      <td><?= $produto['nome'] ?></td>
-      <td><?= $produto['preco'] ?></td>
+      <td><?=$produto['nome'] ?></td>
+      <td><?=$produto['preco'] ?></td>
+      <td><?= substr($produto['descricao'], 0, 40) ?></td>
+      <td>
+        <form action="remove-produto.php?id=<?=$produto['id']?>">
+        <button  class="btn btn-danger">remover</button>
+      </td>
     </tr>
     <?php
       endforeach
